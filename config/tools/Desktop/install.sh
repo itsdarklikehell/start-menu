348e68d7a95f90b037dcf0d2c69fad21ae48104c
Bauke <bauke.molenaar@gmail.com>
2018-06-10 19:11:49 +0200

    modified:   config/install_tool.list 	modified:   config/install_tool.list.old 	modified:   config/start_tool.list 	modified:   config/start_tool.list.old 	modified:   config/tools/Desktop/info/packagename 	modified:   config/tools/Desktop/install.sh 	modified:   config/tools/Desktop/start.sh

--- config/tools/Desktop/install.sh
+++ config/tools/Desktop/install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-name=$(cat $HOME/start-menu/config/tools/Desktop/info/name) # "Desktop"
-packagename=$(cat $HOME/start-menu/config/tools/$name/info/packagename) # ""
-
+name=Desktop
 sudo apt get update
-sudo apt-get install $packagename
+sudo apt-get install pixel-desktop
